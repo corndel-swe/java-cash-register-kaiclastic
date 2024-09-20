4d1f3a1c5ed22b7ea81970db6c2556dca5040933
kaiclastic <halajash567@outlook.com>
2024-09-20 11:30:04 +0100

    solves 2b

--- src/main/java/com/corndel/cashregister/exercises/Exercise2b.java
+++ src/main/java/com/corndel/cashregister/exercises/Exercise2b.java
@@ -1,6 +1,8 @@
 package com.corndel.cashregister.exercises;
 
 import com.corndel.cashregister.exercises.models.Recipe;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise2b {
@@ -10,6 +12,10 @@ public class Exercise2b {
    */
   public static List<String> listNames(List<Recipe> recipes) {
     // TODO
-    return null;
+    List <String> dishNames = new ArrayList<>();
+    for (Recipe dishName: recipes){
+      dishNames.add(dishName.getName());
+    }
+    return dishNames;
   }
 }
